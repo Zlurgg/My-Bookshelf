93224f81353c3ab8d25c8b8869b3c68b012c52f4
Zlurgg <joseph.brightman@gmail.com>
2025-08-11 13:33:52 +0100

    Thinking about the user journey, need a screen that handles the searches, can search from any screen -> returns list of books can then go to the details page option to add to bookshelf either existing or create new.

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_list/BookListAction.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_list/BookListAction.kt
@@ -1,4 +1,8 @@
 package uk.co.zlurgg.mybookshelf.bookshelf.presenation.book_list
 
-interface BookListAction {
+import uk.co.zlurgg.mybookshelf.bookshelf.domain.book.Book
+
+sealed interface BookListAction {
+    data class OnSearchQueryChange(val query: String): BookListAction
+    data class OnBookClick(val book: Book): BookListAction
 }
