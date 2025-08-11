92f8e1eeb4ed4c5abea1134840c96428b4910d88
Zlurgg <joseph.brightman@gmail.com>
2025-08-11 17:02:55 +0100

    search bar screen and list view for book items

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_search/BookSearchViewModel.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_search/BookSearchViewModel.kt
@@ -3,10 +3,24 @@ package uk.co.zlurgg.mybookshelf.bookshelf.presenation.book_search
 import androidx.lifecycle.ViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.update
 
 class BookSearchViewModel: ViewModel() {
 
     private val _state = MutableStateFlow(BookSearchState())
     val state: StateFlow<BookSearchState> = _state
 
+    fun onAction(action: BookSearchAction) {
+        when (action) {
+            is BookSearchAction.OnBookClick -> {
+
+            }
+            is BookSearchAction.OnSearchQueryChange -> {
+                _state.update {
+                    it.copy(searchQuery = action.query)
+                }
+            }
+
+        }
+    }
 }
