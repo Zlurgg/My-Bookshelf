d925f08ea43ba60113b843e60633c85940275f42
Zlurgg <joseph.brightman@gmail.com>
2025-08-11 13:33:52 +0100

    Thinking about the user journey, need a screen that handles the searches, can search from any screen -> returns list of books can then go to the details page option to add to bookshelf either existing or create new.

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_list/BookListState.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_list/BookListState.kt
@@ -3,7 +3,7 @@ package uk.co.zlurgg.mybookshelf.bookshelf.presenation.book_list
 import uk.co.zlurgg.mybookshelf.bookshelf.domain.book.Book
 
 data class BookListState(
-    val searchQuery: String = "Kotlin",
+    val searchQuery: String = "Programming Books",
     val searchResults: List<Book> = emptyList(),
     val isLoading: Boolean = true,
     val selectedTabIndex: Int = 0,
