31e54c98c636fa9aab29eafaf66e46b31ef22f3d
Zlurgg <joseph.brightman@gmail.com>
2025-08-26 14:12:08 +0100

    bookcase setup now creating bookshelf via dialog.
    BookcaseRepoImpl currently just creating bookshelf in memory pre using database and api.

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/bookshelf/repository/BookshelfRepository.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/bookshelf/repository/BookshelfRepository.kt
@@ -2,7 +2,6 @@ package uk.co.zlurgg.mybookshelf.bookshelf.domain.bookshelf.repository
 
 import kotlinx.coroutines.flow.Flow
 import uk.co.zlurgg.mybookshelf.bookshelf.domain.book_detail.Book
-import uk.co.zlurgg.mybookshelf.bookshelf.domain.bookshelf.Bookshelf
 
 interface BookshelfRepository {
     suspend fun searchBooks(query: String): List<Book>
