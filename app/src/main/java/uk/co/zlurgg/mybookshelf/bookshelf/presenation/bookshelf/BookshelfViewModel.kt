385f102926888d728221ccae5018bfefe9296193
Zlurgg <joseph.brightman@gmail.com>
2025-08-27 17:47:24 +0100

    naviagtion to bookshelf working from screen

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/bookshelf/BookshelfViewModel.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/bookshelf/BookshelfViewModel.kt
@@ -5,7 +5,6 @@ import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
-import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import uk.co.zlurgg.mybookshelf.bookshelf.domain.book_detail.Book
