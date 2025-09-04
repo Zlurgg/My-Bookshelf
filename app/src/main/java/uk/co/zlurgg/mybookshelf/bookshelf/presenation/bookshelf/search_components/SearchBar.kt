0ee7591a09fd53d5c340b34462e44c7fd6d9c95b
Zlurgg <joseph.brightman@gmail.com>
2025-09-04 12:01:09 +0100

    shared viewmodel, to keep ssot allow for name of shelf to be displayed on the bookshelf page.

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/bookshelf/search_components/SearchBar.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/bookshelf/search_components/SearchBar.kt
@@ -20,11 +20,10 @@ import androidx.compose.material3.minimumInteractiveComponentSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
-import uk.co.zlurgg.mybookshelf.R
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
+import uk.co.zlurgg.mybookshelf.R
 
 
 @Composable
