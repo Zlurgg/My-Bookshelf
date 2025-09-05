ff940aaacc1fb8eb0740d137c1ad6e5b3c7ac665
Zlurgg <joseph.brightman@gmail.com>
2025-09-05 14:21:11 +0100

    dead file removed, added a proper todo for purchase fixed rating functionality

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_detail/BookDetailAction.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presenation/book_detail/BookDetailAction.kt
@@ -3,7 +3,7 @@ package uk.co.zlurgg.mybookshelf.bookshelf.presenation.book_detail
 import uk.co.zlurgg.mybookshelf.bookshelf.domain.Book
 
 sealed interface  BookDetailAction {
-    data object OnRateBookDetailClick : BookDetailAction
+    data class OnRateBookDetailClick(val rating: Int) : BookDetailAction
     data object OnPurchaseClick : BookDetailAction
     data class OnAddBookClick(val book: Book) : BookDetailAction
     data class  OnRemoveBookClick(val book: Book): BookDetailAction
