141ba5da1f7b50c24803390769f2554d90ee0d6e
Zlurgg <joseph.brightman@gmail.com>
2025-09-08 21:14:24 +0100

    changed thickness

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/bookcase/components/BookshelfCard.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/bookcase/components/BookshelfCard.kt
@@ -48,7 +48,7 @@ fun BookshelfCard(
             containerColor = MaterialTheme.colorScheme.surface
         ),
         border = BorderStroke(
-            width = 12.dp,
+            width = 8.dp,
             color = getShelfStyleColor(shelf.shelfStyle)
         ),
         elevation = CardDefaults.cardElevation(defaultElevation = 2.dp)
