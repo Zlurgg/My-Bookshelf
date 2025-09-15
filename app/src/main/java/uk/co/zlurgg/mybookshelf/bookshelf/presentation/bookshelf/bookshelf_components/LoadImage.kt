953b173b79b9a435ceb281ce0c3e831ce9135c78
Zlurgg <joseph.brightman@gmail.com>
2025-09-15 17:16:45 +0100

    coil3 update, requires ktor adding to do the network part, image factory for config of coil3
    currently having an issue with alot of the images not loading - openbooks is sending me to archive.org
    
    not sure if this is an issue on their end
    
    removed the error image because in the case of a missing image its not an error just no image and it looks messy.
    
    book details is trying to check for image and if not able to load one collapse the space

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/bookshelf/bookshelf_components/LoadImage.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/bookshelf/bookshelf_components/LoadImage.kt
@@ -1,21 +1,15 @@
 package uk.co.zlurgg.mybookshelf.bookshelf.presentation.bookshelf.bookshelf_components
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil3.compose.SubcomposeAsyncImage
-import uk.co.zlurgg.mybookshelf.R
 
 @Composable
 fun LoadImage(
