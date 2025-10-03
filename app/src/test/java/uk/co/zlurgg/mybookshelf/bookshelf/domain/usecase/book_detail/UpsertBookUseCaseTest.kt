9ffc726cdc73dae303817dfe7c99bc071ace92d5
Zlurgg <joseph.brightman@gmail.com>
2025-10-03 14:49:10 +0100

    redundant errors removed

--- app/src/test/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/usecase/book_detail/UpsertBookUseCaseTest.kt
+++ app/src/test/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/usecase/book_detail/UpsertBookUseCaseTest.kt
@@ -82,8 +82,7 @@ class UpsertBookUseCaseTest {
 
         // Then
         assertTrue("Should return error", result is Result.Error)
-        val error = (result as Result.Error).error
-        assertTrue("Should be DataError.Local", error is DataError.Local)
+        // Error is correctly typed as DataError.Local after unwrapping Result.Error
     }
 
     @Test
