7fbdd79ec1eadada41d34089f0aa5f16dd7e19be
Zlurgg <joseph.brightman@gmail.com>
2025-10-07 16:35:00 +0100

    removing unused fields from welcome state - keeping it so we are consistent

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/welcome/WelcomeState.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/welcome/WelcomeState.kt
@@ -1,5 +1,8 @@
 package uk.co.zlurgg.mybookshelf.bookshelf.presentation.welcome
 
-data class WelcomeState(
-    val isLoading: Boolean = false
-)
+/**
+ * State for the Welcome screen.
+ * Currently has no state fields but maintained for architectural consistency and future extensibility.
+ * Potential future fields: error states, DataStore write confirmation, loading states, etc.
+ */
+class WelcomeState
