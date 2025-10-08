b0b92c85f37a617bec626b15ab6ee048699a9278
Zlurgg <joseph.brightman@gmail.com>
2025-10-08 15:58:50 +0100

    ui updates: Keep search open Hide purchase button Fix share success

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/book_detail/components/BookDetailActionButtons.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/presentation/book_detail/components/BookDetailActionButtons.kt
@@ -6,7 +6,6 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
-import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
