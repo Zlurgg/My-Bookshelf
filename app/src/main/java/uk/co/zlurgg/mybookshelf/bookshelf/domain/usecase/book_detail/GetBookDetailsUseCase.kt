7bbe885e38eca7281570120b6f17e2ae0eea6ae6
Zlurgg <joseph.brightman@gmail.com>
2025-10-13 15:27:33 +0100

    few fixes and tidies

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/usecase/book_detail/GetBookDetailsUseCase.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/usecase/book_detail/GetBookDetailsUseCase.kt
@@ -2,16 +2,9 @@ package uk.co.zlurgg.mybookshelf.bookshelf.domain.usecase.book_detail
 
 import kotlinx.coroutines.flow.Flow
 import uk.co.zlurgg.mybookshelf.bookshelf.domain.model.Book
+import uk.co.zlurgg.mybookshelf.bookshelf.domain.model.BookDetailsWithShelfStatus
 import uk.co.zlurgg.mybookshelf.core.domain.error.DataError
 import uk.co.zlurgg.mybookshelf.core.domain.result.Result
-
-/**
- * Data class representing complete book details including shelf membership
- */
-data class BookDetailsWithShelfStatus(
-    val book: Book?,
-    val isOnShelf: Boolean
-)
 
 /**
  * UseCase for getting comprehensive book details including shelf membership.
--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/usecase/book_detail/GetBookDetailsUseCaseImpl.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/usecase/book_detail/GetBookDetailsUseCaseImpl.kt
@@ -2,6 +2,7 @@ package uk.co.zlurgg.mybookshelf.bookshelf.domain.usecase.book_detail
 
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.combine
+import uk.co.zlurgg.mybookshelf.bookshelf.domain.model.BookDetailsWithShelfStatus
 import uk.co.zlurgg.mybookshelf.bookshelf.domain.repository.BookRepository
 import uk.co.zlurgg.mybookshelf.bookshelf.domain.repository.BookshelfRepository
 import uk.co.zlurgg.mybookshelf.core.domain.error.DataError
