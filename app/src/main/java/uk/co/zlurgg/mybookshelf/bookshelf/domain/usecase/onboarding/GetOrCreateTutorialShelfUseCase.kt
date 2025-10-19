3cee347af2a9d93189d67e9a0647597dd98437ec
Zlurgg <joseph.brightman@gmail.com>
2025-10-19 16:41:07 +0100

    naming for welcome and onboarding tidy - we don't really onboard its just a welcome so bit of rework to keep it clean

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/domain/usecase/onboarding/GetOrCreateTutorialShelfUseCase.kt
+++
@@ -1,8 +0,0 @@
-package uk.co.zlurgg.mybookshelf.bookshelf.domain.usecase.onboarding
-
-import uk.co.zlurgg.mybookshelf.core.domain.error.DataError
-import uk.co.zlurgg.mybookshelf.core.domain.result.Result
-
-interface GetOrCreateTutorialShelfUseCase {
-    suspend fun execute(): Result<String, DataError.Local>
-}
