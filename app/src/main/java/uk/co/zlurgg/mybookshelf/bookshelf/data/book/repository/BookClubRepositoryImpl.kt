1dd7980152b8e04050aa30c3725aac998ef27c26
Zlurgg <joseph.brightman@gmail.com>
2025-12-19 15:13:09 +0000

    throwable goes first

--- app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/data/book/repository/BookClubRepositoryImpl.kt
+++ app/src/main/java/uk/co/zlurgg/mybookshelf/bookshelf/data/book/repository/BookClubRepositoryImpl.kt
@@ -517,7 +517,7 @@ class BookClubRepositoryImpl(
                 bookshelfDao.upsertCrossRef(crossRef)
                 addedCount++
             } catch (e: Exception) {
-                Timber.tag(TAG).w("Failed to add book %s to local shelf: %s", bookDto.id, e)
+                Timber.tag(TAG).w(e, "Failed to add book %s to local shelf", bookDto.id)
                 // Continue with other books
             }
         }
