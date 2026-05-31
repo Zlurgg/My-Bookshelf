c91fc26323a8edc7cf6594c9dcbfbfba86abed0b
Zlurgg <joseph.brightman@gmail.com>
2026-05-31 20:29:49 +0100

    chore(release): bump versionCode to 4 for Firebase key rotation re-upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ android {
         applicationId = "uk.co.zlurgg.mybookshelf"
         minSdk = 28
         targetSdk = 36
-        versionCode = 3
+        versionCode = 4
         versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
